e57c663dee9c4a2df5a893e48071fe463f4f2a0b
bintang25a <bintangalfizar25@gmail.com>
2026-05-01 11:55:20 +0700

    1 Mei 2026

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+  return (
+    <main>
+      <h1>keren</h1>
+    </main>
+  );
+}
